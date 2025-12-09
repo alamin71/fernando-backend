10bc57a1f0c77f00df3207265f12a7a59adcffb1
Al-Amin <alamin50cse.miu@gmail.com>
2025-12-09 07:08:06 +0600

    admin created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { UserRouter } from "../app/modules/user/user.route";
 import AuthRouter from "../app/modules/auth/auth.route";
+import { adminRoutes } from "../app/modules/admin/admin.route";
 
 const router = express.Router();
 const routes = [
@@ -11,6 +12,10 @@ const routes = [
   {
     path: "/users",
     route: UserRouter,
+  },
+  {
+    path: "/admin",
+    route: adminRoutes,
   },
 ];
 
