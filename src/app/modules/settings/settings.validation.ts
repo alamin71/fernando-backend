16d406df4d73c8b1ea7cdc5042e1a53fe531bdfe
Al-Amin <alamin50cse.miu@gmail.com>
2025-12-14 04:43:11 +0600

    updated setting

--- src/app/modules/settings/settings.validation.ts
+++ src/app/modules/settings/settings.validation.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 const updateContentValidation = z.object({
-  body: z.object({
-    content: z.string().min(1, "Content is required"),
-  }),
+  content: z.string().min(1, "Content is required"),
 });
 
 export const settingsValidation = {
